cb970075582df8b777bf8876d37af7f0bed2ef92
25LP7865 25LP7865 <dhulan174@gmail.com>
2026-01-19 16:18:02 +0800

    nsmxn

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -14,6 +14,7 @@ type Movie = {
   id: number;
   title: string;
   poster_path: string;
+  vote_average: number;
 };
 
 type Genre = {
@@ -65,22 +66,43 @@ export default function GenrePage() {
           <h1 className="text-2xl font-semibold mb-6">
             {movies.length} titles in “{genreName}”
           </h1>
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {movies.map((movie) => (
-              <div key={movie.id} className="text-center">
+              <div
+                key={movie.id}
+                className="group bg-white rounded-xl  overflow-hidden hover:shadow-md transition"
+              >
+                {/* Poster */}
                 {movie.poster_path && (
                   <Image
                     src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     alt={movie.title}
-                    width={260}
-                    height={390}
-                    className="rounded-lg shadow-md mx-auto"
+                    width={300}
+                    height={450}
+                    className="w-full object-cover"
                   />
                 )}
-                <p className="mt-2 text-sm font-medium">{movie.title}</p>
+
+                {/* Info */}
+                <div className="p-3 bg-gray-300">
+                  {/* ⭐ Rating */}
+                  <div className="flex items-center gap-1 text-sm mb-1">
+                    <span className="text-yellow-500">★</span>
+                    <span className="font-medium">
+                      {movie.vote_average.toFixed(1)}
+                    </span>
+                    <span className="text-gray-400">/10</span>
+                  </div>
+
+                  {/* Title */}
+                  <p className="text-sm font-medium text-gray-900 leading-snug line-clamp-2">
+                    {movie.title}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
+
           {/* ===== Pagination =====
           <div className="flex justify-center items-center gap-2 mt-10 text-sm">
             <button
